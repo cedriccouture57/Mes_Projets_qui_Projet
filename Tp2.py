ba5fba7f9e69aa73d2de861edb45fb0e89c4963b
couturec <couturec@educ.college-montreal.qc.ca>
2025-09-11 13:09:45 -0400

    initial hahaha

--- Tp2.py
+++ Tp2.py
@@ -4,19 +4,19 @@ Code pour un jeu de devinette de chiffre de 1:1000
 -Cédric Couture
 
 '''
-
+print("Essayez de deviner le chiffre que j'ai choisi hihihi")
 import random
 devinette = random.randint(1, 1000)
 binouche = int(input('Réponse :'))
 while binouche != devinette :
 
-    if binouche == devinette :
-        print(f'Brazooooo! Le Numéro étais bien {devinette}')
-
-    elif binouche <= devinette :
+    if binouche <= devinette :
         print("Bah non c'est plus grand hehehe ")
 
     else :
         print("Bah non c'est plus petit hehehe ")
 
     binouche = int(input('Réponse :'))
+
+if binouche == devinette:
+    print(f'Brazooooo! Le numéro étais bien {devinette}')
