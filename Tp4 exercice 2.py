89172c722142858592b9828939b86671da255d0f
couturec <couturec@educ.college-montreal.qc.ca>
2025-12-03 16:15:22 -0500

    Pluh

---
+++ Tp4 exercice 2.py
@@ -0,0 +1,17 @@
+import random
+
+class Npc:
+    def __init__(self):
+        self.force =
+        self.agilite =
+        self.constitution =
+        self.intelligence =
+        self.sagesse =
+        self.charisme =
+
+
+def binouche():
+    de = [random.randint(1,6), random.randint(1,6), random.randint(1,6), random.randint(1,6)]
+    de.sort(reverse=True)
+
+    pouette =  de[0] + de[1] + de[2]
