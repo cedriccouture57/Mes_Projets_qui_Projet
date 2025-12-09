15f19660a9d0278c02076673187e7b74b6bba95e
couturec <couturec@educ.college-montreal.qc.ca>
2025-12-09 12:02:32 -0500

    Pluh 2

--- Tp4 exercice 2.py
+++ Tp4 exercice 2.py
@@ -1,17 +1,36 @@
 import random
-
+pouette = int
 class Npc:
-    def __init__(self):
-        self.force =
-        self.agilite =
-        self.constitution =
-        self.intelligence =
-        self.sagesse =
-        self.charisme =
-
+    def __init__(self, force, agilite,constitution, intelligence, sagesse, charisme):
+        self.force = force
+        self.agilite = agilite
+        self.constitution = constitution
+        self.intelligence = intelligence
+        self.sagesse = sagesse
+        self.charisme = charisme
+        self.armure = random.randint(1,12)
+        self.nom = "poteto"
+        self.race = "acier inoxydable"
+        self.espece = "porte"
+        self.pv = random.randint(1,20)
+        self.profession = "portier"
 
 def binouche():
     de = [random.randint(1,6), random.randint(1,6), random.randint(1,6), random.randint(1,6)]
     de.sort(reverse=True)
 
-    pouette =  de[0] + de[1] + de[2]
+    pouette = de[0] + de[1] + de[2]
+
+binouche()
+Npc.force = pouette
+binouche()
+Npc.agilite = pouette
+binouche()
+Npc.constitution = pouette
+binouche()
+Npc.intelligence = pouette
+binouche()
+Npc.sagesse = pouette
+binouche()
+Npc.charisme = pouette
+
