442db2add1d68df6e449c63ae3f1bcae2b656ec7
couturec <couturec@educ.college-montreal.qc.ca>
2026-01-06 12:09:01 -0500

    Pluh 2

--- Tp4 exercice 2.py
+++ Tp4 exercice 2.py
@@ -1,42 +1,50 @@
 import random
-pouette = int
+
+
+def calcul_stats():
+    de = [random.randint(1,6), random.randint(1,6), random.randint(1,6), random.randint(1,6)]
+    de.sort(reverse=True)
+
+    return de[0] + de[1] + de[2]
+
+
 class Npc:
-    def __init__(self, force, agilite ,constitution, intelligence, sagesse, charisme):
-        self.force = force
-        self.agilite = agilite
-        self.constitution = constitution
-        self.intelligence = intelligence
-        self.sagesse = sagesse
-        self.charisme = charisme
+    def __init__(self):
+        self.force = calcul_stats()
+        self.agilite = calcul_stats()
+        self.constitution = calcul_stats()
+        self.intelligence = calcul_stats()
+        self.sagesse = calcul_stats()
+        self.charisme = calcul_stats()
         self.armure = random.randint(1,12)
-        self.nom = "poteto"
-        self.race = "acier inoxydable"
-        self.espece = "porte"
+        self.nom = ""
+        self.race = ""
+        self.espece = ""
         self.pv = random.randint(1,20)
-        self.profession = "portier"
+        self.profession = ""
 
     def afficher_stats(self):
         print(f"Force = {self.force} agillite = {self.agilite} constitution = {self.constitution} intelligence = {self.intelligence} sagesse = {self.sagesse} charisme = {self.charisme}")
 
 
+n = Npc()
+n.afficher_stats()
 
 
-def binouche():
-    de = [random.randint(1,6), random.randint(1,6), random.randint(1,6), random.randint(1,6)]
-    de.sort(reverse=True)
+class Kobold(Npc):
+    def __init__(self, nom):
+        super().__init__()
+        self.race = "Kobold"
+        self.nom = nom
 
-    pouette = de[0] + de[1] + de[2]
+    def afficher_stats(self):
+        print(f"Nom = {self.nom}")
+        super().afficher_stats()
 
-binouche()
-Npc.force = pouette
-binouche()
-Npc.agilite = pouette
-binouche()
-Npc.constitution = pouette
-binouche()
-Npc.intelligence = pouette
-binouche()
-Npc.sagesse = pouette
-binouche()
-Npc.charisme = pouette
+
+
+# faire hero et cible la meme chose que kobold pouette binouche touppi et binou
+
+k = Kobold("Binouche")
+k.afficher_stats()
 
