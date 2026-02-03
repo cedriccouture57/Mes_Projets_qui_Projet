aa91a8b7271d7ff4f9d1846bb7403bbd2b087ff6
couturec <couturec@educ.college-montreal.qc.ca>
2026-02-03 12:10:10 -0500

    Pluh 2

--- Tp4 exercise 3.py
+++ Tp4 exercise 3.py
@@ -1,15 +1,30 @@
 from dataclasses import dataclass
 import random
 @dataclass
-class liste:
-    piere: int = 4
+class Item:
+    nom: str
+    qte: int
 
 
-
-
-
-
-
-class sac_a_dos:
+class SacADos:
     def __init__(self):
         self.liste_item = []
+
+    def ajouter_item(self, item_a_ajouter: Item):
+        if len(self.liste_item) == 0:
+            # vide, donc ajouter sans vérification
+            self.liste_item.append(item_a_ajouter)
+        else:
+            for item in self.liste_item:
+                if item.nom == item_a_ajouter.nom:
+                    item.qte += item_a_ajouter.qte
+                else :
+                    self.liste_item.append(item_a_ajouter)
+
+
+sad = SacADos()
+sad.ajouter_item(Item("or", 5))
+sad.ajouter_item(Item("or", 10))
+sad.ajouter_item(Item("argent", 10))
+
+print(sad.liste_item)
